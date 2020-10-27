5d1c32edf3b2b5fc4bae600d3daa1f3d92822de9
edz <edz@sunliang.local>
2020-10-27 19:46:27 +0800

    css

--- src/App.vue
+++ src/App.vue
@@ -14,23 +14,23 @@
 
 <style lang="scss">
 #app {
-  font-family : Avenir, Helvetica, Arial, sans-serif;
-  color : #2c3e50;
-  text-align : center;
-  -webkit-font-smoothing : antialiased;
-  -moz-osx-font-smoothing : grayscale;
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    color: #2c3e50;
+    text-align: center;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
 }
 
 #nav {
-  padding : 30px;
+    padding: 30px;
 
-  a {
-    font-weight : bold;
-    color : #2c3e50;
+    a {
+        font-weight: bold;
+        color: #2c3e50;
 
-    &.router-link-exact-active {
-      color : #42b983;
+        &.router-link-exact-active {
+            color: #42b983;
+        }
     }
-  }
 }
 </style>
