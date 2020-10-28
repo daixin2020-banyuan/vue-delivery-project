0495632cfb30a0de40a08ab0e6c48a884011f10b
edz <edz@edzdeMacBook-Air-6.local>
2020-10-28 18:37:17 +0800

    header end

--- src/components/errorModal/errorModal.vue
+++ src/components/errorModal/errorModal.vue
@@ -25,33 +25,33 @@ export default {
 
       };
    },
-  
+
 };
 </script>
 
 <style lang="scss">
 
 .error-div{
-   width: 400px;
-   height: 100px;
-   padding:10px;
-  text-align: center;
-  display: flex;
-  flex-direction:column;
-  justify-content: center;
+  display : flex;
+  flex-direction :column;
+  justify-content : center;
+   width : 400px;
+   height : 100px;
+   padding :10px;
+  text-align : center;
     .error-but{
 
-        width: 64px;
-        height: 30px;
-        margin-top: 30px;
-        padding: 5px 18px;
-        font-size: 14px;
-        color: #fff;
-        background: #202020;
-        border: 0;
-        border-radius: 25px;
-        margin: 0 auto;
-        outline: none;
+        width : 64px;
+        height : 30px;
+        padding : 5px 18px;
+        margin : 0 auto;
+        margin-top : 30px;
+        font-size : 14px;
+        color : #fff;
+        background : #202020;
+        border : 0;
+        border-radius : 25px;
+        outline : none;
     }
 }
 .vm--modal{
