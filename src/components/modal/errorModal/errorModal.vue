fa4f3b90faf26d47c0df87e73d53b73b9d67fe25
tang dian <edz@edzdeMacBook-Air-3.local>
2020-10-28 09:44:54 +0800

    modal

---
+++ src/components/modal/errorModal/errorModal.vue
@@ -0,0 +1,34 @@
+
+<template>
+  <div>
+    <button @click="showModal">
+      showModal
+    </button>
+  </div>
+</template>
+
+<script>
+
+export default {
+   data (){
+      return{
+
+      };
+   },
+   methods:{
+      showModal (){
+         this.$modal.show( {
+            text: 'This text is passed as a property'
+         }, {
+            draggable: true,
+            clickToClose: false
+         });
+
+      }
+   }
+};
+</script>
+
+<style lang="scss">
+
+</style>
